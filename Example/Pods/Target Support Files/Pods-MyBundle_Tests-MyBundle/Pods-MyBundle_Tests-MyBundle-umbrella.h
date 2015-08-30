f8daeb78b5b0a5ef964b107cb26521160a817256
LiuShulong <bigfish_liu@126.com>
2015-08-30 23:22:39 +0800

    init pod

---
+++ Example/Pods/Target Support Files/Pods-MyBundle_Tests-MyBundle/Pods-MyBundle_Tests-MyBundle-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double MyBundleVersionNumber;
+FOUNDATION_EXPORT const unsigned char MyBundleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-MyBundle_Tests/Pods-MyBundle_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyBundle_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MyBundle_Tests
+@end
